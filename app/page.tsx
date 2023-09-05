a244dae3e7d224bc71afbd8c932c0dea05ee8fa8
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-09-05 18:11:15 +0100

    listArrays.map

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,21 @@ function Item({ name, isPacked }) {
 	}
 	return <li className="item">{name}</li>;
   }
+
+  const people = [
+	'Creola Katherine Johnson: mathematician',
+	'Mario José Molina-Pasquel Henríquez: chemist',
+	'Mohammad Abdus Salam: physicist',
+	'Percy Lavon Julian: chemist',
+	'Subrahmanyan Chandrasekhar: astrophysicist'
+  ];
+  
+   function List() {
+	const listItems = people.map(person =>
+	  <li>{person}</li>
+	);
+	return <ul>{listItems}</ul>;
+  }
   
   export default function PackingList() {
 	return (
